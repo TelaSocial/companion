92937640e65607302269f4b1910b4b792b22a46c
Fabricio C Zuardi <fabricio@fabricio.org>
2014-04-08 13:50:06 -0300

    added new task for the cordova:setup task, the platform add android one

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
 
 //libraries
 var pkg = require('./package.json'),
+    path = require('path'),
+    mkdirp = require('mkdirp'),
     gulp = require('gulp'),
+    gutil = require('gulp-util'),
     cordova = require('cordova'),
     CordovaError = require('cordova/src/CordovaError'),
-    gutil = require('gulp-util');
+    Q = require('q');
 
 //paths
 var paths = {
@@ -16,40 +19,61 @@ var paths = {
             data: 'src/data/*.json'
         },
         build: {
+            root: 'dist',
             www: 'dist/www/',
             cordova: 'dist/cordova/',
         }
     };
 
 //globals
-var magenta = gutil.colors.magenta,
-    red = gutil.colors.red,
+var red = gutil.colors.red,
+    // magenta = gutil.colors.magenta,
     // cyan = gutil.colors.cyan,
-    // blue = gutil.colors.blue,
-    green = gutil.colors.green;
+    // green = gutil.colors.green,
+    blue = gutil.colors.blue;
 
 //----------------------------------------------------------------------------
 // tasks
 //----------------------------------------------------------------------------
 
+gulp.task('createBuildDir', function(cb){
+    mkdirp(paths.build.root, function(err) {
+        if(err){ throw err; }
+        cb(null);
+    });
+});
+
+var cordovaLog = function(msg){
+    console.log('[' + blue('cordova')+ '] ', msg);
+};
+
 //creates a cordova folder for the app
-gulp.task('cordova:create', function(){
+gulp.task('cordova:create', ['createBuildDir'], function(cb){
     var name = pkg.name,
         dir = paths.build.cordova,
         id = pkg.cordovaConfig.id,
-        cfg = pkg.cordovaConfig.extra;
+        cfg = pkg.cordovaConfig.extra,
+        promise;
 
-    gutil.log(
-        'Setting up', magenta(name),'(' + green(id) + ') at', magenta(dir) + '...'
-    );
-    cordova.create(dir, id, name, cfg);
+    cordova.on('log', cordovaLog);
+    cordova.on('verbose', cordovaLog);
+    promise = new Q(cordova.create(dir, id, name, cfg));
+    promise.then(function(){
+        gutil.log('cordova create finished.');
+        cb(null);
+    });
+});
+
+gulp.task('cordova:addAndroid', ['cordova:create'], function(){
+    process.env.PWD = path.join(process.env.PWD, paths.build.cordova);
+    console.log('process.env.PWD', process.env.PWD);
+    cordova.platform('add', 'android');
 });
 
 //run all the initial cordova-related tasks
 gulp.task('cordova:setup',
     [
-        'cordova:create'
-// - cordova platform add android
+        'cordova:addAndroid'
 // - cordova plugin add https://github.com/EddyVerbruggen/Calendar-PhoneGap-Plugin.git
 // - cordova plugin add https://github.com/Red-Folder/bgs-core
 // - cordova plugin add https://github.com/Red-Folder/bgs-sample
