94bdbdd393a2bd449de92597e35a8ca202a6ca41
Fabricio C Zuardi <fabricio@fabricio.org>
2014-04-20 02:02:02 -0300

    separated cordova calendar functions into its own file

--- src/scripts/lib/companion/app.js
+++ src/scripts/lib/companion/app.js
@@ -1,46 +1,13 @@
 'use strict';
 
+var cordovaCalendarHelper = require('./cordova_calendar');
+
 module.exports = function($, FISLParser, templates){
-    var isCordova = document.URL.substring(0,4) === 'file';
-
-    var addToCalendarButtonClicked = function (event){
-        var button = $(this),
-            sessionItem = button.parents('.session').first(),
-            sessionTitle = sessionItem.find('.session-title').text().trim(),
-            sessionAbstract = sessionItem.find('.session-abstract').text().trim(),
-            start = sessionItem.data('start'),
-            duration = sessionItem.data('duration'), //minutes
-            roomId = sessionItem.data('room'),
-            roomName = $('#'+roomId).text(),
-            title = sessionTitle,
-            location = 'Sala: ' + roomName + ' (FISL 15)',
-            notes = sessionAbstract,
-            startDate = new Date(start),
-            endDate = new Date(startDate.getTime() + duration * 60 * 1000),
-            success = function(message) { console.log('Success: ' + JSON.stringify(message)); },
-            error = function(message) { console.log('Error: ' + message); };
-        event.preventDefault();
-        console.log('addToCalendarButtonClicked');
-        console.log(sessionItem);
-        console.log(title);
-        console.log(location);
-        console.log(notes);
-        console.log(startDate);
-        console.log(endDate);
-
-        if ((window.plugins !== undefined) && (window.plugins.calendar !== undefined)){
-            // create an event silently (on Android < 4 an interactive dialog is shown)
-            // window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
-
-            // create an event interactively (only supported on Android)
-            window.plugins.calendar.createEventInteractively(title,location,notes,startDate,endDate,success,error);
-        } else {
-            console.log('Add to calender not supported in your platform');
-        }
-    };
+    var isCordova = document.URL.substring(0,4) === 'file',
+        cordovaFunctions = new cordovaCalendarHelper($);
 
     var setupAddToCalendarButtons = function(){
-        $('.calendar-add-button').click(addToCalendarButtonClicked);
+        $('.calendar-add-button').click(cordovaFunctions.addToCalendarButtonClicked);
     };
 
     var populateSchedule = function(data){
---
+++ src/scripts/lib/companion/cordova_calendar.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function($){
+    this. addToCalendarButtonClicked = function (event){
+        var button = $(this),
+            sessionItem = button.parents('.session').first(),
+            sessionTitle = sessionItem.find('.session-title').text().trim(),
+            sessionAbstract = sessionItem.find('.session-abstract').text().trim(),
+            start = sessionItem.data('start'),
+            duration = sessionItem.data('duration'), //minutes
+            roomId = sessionItem.data('room'),
+            roomName = $('#'+roomId).text(),
+            title = sessionTitle,
+            location = 'Sala: ' + roomName + ' (FISL 15)',
+            notes = sessionAbstract,
+            startDate = new Date(start),
+            endDate = new Date(startDate.getTime() + duration * 60 * 1000),
+            success = function(message) { console.log('Success: ' + JSON.stringify(message)); },
+            error = function(message) { console.log('Error: ' + message); };
+        event.preventDefault();
+        console.log('addToCalendarButtonClicked');
+        console.log(sessionItem);
+        console.log(title);
+        console.log(location);
+        console.log(notes);
+        console.log(start);
+        console.log(duration);
+        console.log(startDate);
+        console.log(endDate);
+
+        if ((window.plugins !== undefined) && (window.plugins.calendar !== undefined)){
+            // create an event silently (on Android < 4 an interactive dialog is shown)
+            // window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
+
+            // create an event interactively (only supported on Android)
+            window.plugins.calendar.createEventInteractively(title,location,notes,startDate,endDate,success,error);
+        } else {
+            console.log('Add to calender not supported in your platform');
+        }
+    };
+};
